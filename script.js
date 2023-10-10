b8e3993a59711e27412e7f16b565c1f2b7ecab6c
Belma Korlat <137005711+BelmaKorlat@users.noreply.github.com>
2023-10-10 21:45:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var krugic = document.getElementsByClassName("circle2");
+document.onmousemove = function () {
+    var x = event.clientX * 50 / window.innerWidth + "%";
+    var y = event.clientY * 50 / window.innerHeight + "%";
+
+    for (var i = 0; i < 2; i++) {
+        krugic[i].style.left = x;
+        krugic[i].style.top = y;
+        krugic[i].style.transform = "translate(-" + x + ", -" + y + ")";
+    }
+}
