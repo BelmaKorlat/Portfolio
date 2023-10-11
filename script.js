1eafc2610b58ac2de137a39d4e7b99c979f06a41
Belma Korlat <137005711+BelmaKorlat@users.noreply.github.com>
2023-10-11 12:48:42 +0200

    Making changes to the navigation and the header part

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ document.onmousemove = function () {
     for (var i = 0; i < 2; i++) {
         krugic[i].style.left = x;
         krugic[i].style.top = y;
-        krugic[i].style.transform = "translate(-" + x + ", -" + y + ")";
+        krugic[i].style.transform = "translate(" + x + ", " + y + ")";
     }
 }
+
+function resetPage() {
+    // Add your code here to reset the page to its initial state
+    // For example, you can clear form inputs, reset variables, etc.
+    // Here, we are just reloading the page for demonstration purposes.
+    location.reload();
+}
+
+// Get a reference to the reset button
+var reset = document.getElementsByClassName("circle1");
+
+// Add an onclick event handler to the button
+reset.onclick = resetPage;
